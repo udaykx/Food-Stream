8a070eedbe99bc87f58412e3edd844669a1b6d9a
uday <udaykumar2600@gmail.com>
2026-08-13 02:17:17 +0530

    MongoDB Connection

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+const userModel = require('../models/user.model')
+
+
+async function registerUser(req, res) {
+    const { fullName, eamil, password } = req.body;
+
+    const isUserAlreadyExists = await userModel.findOne({
+        email
+    })
+
+    if(isUserAlreadyExists){
+        return res.status(400).json({
+            message:"User already exists"
+        })
+    }
+}
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { type } = require('node:os');
+
+
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+
+    }
+},
+    {
+        timestamps: true
+    }
+    
+)
+
+const userModel = mongoose.model("user", userSchema); 
+
+module.exports = userModel;
