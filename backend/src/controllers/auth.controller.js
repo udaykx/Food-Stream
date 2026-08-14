de14b93a4bc58129f6d292acf6ecffa05fce250c
uday <udaykumar2600@gmail.com>
2026-08-15 01:16:58 +0530

    create logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -39,9 +39,50 @@ async function registerUser(req, res) {
     })
 }
 
-async function loginUser(req, res) { }
+async function loginUser(req, res) { 
+    const { email,password } = req.body;
+    const user = await userModel.findOne({
+        email
+    })
+
+    if(!user){
+        res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid) {
+        return res.status(400).json({
+            message: "Invalid email or password"
+        })
+    }
+
+    const token = jwt.sign ({
+        id: user._id,
+    }, process.env.JWT_SECRET)
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        user: {
+            _id: user._id,
+            email: user.email,
+            fullName: user.fullName
+        }
+    })
+}
+
+async function logoutUser(req, res) {
+    res.clearCookie("token");
+    res.status(200).json({
+        message: "User logged out successfully"
+    });
+}
 
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
