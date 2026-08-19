c0eac0127981a0f2ee9574c4707e2b948fde3548
uday <udaykumar2600@gmail.com>
2026-08-19 23:16:26 +0530

    food partner Schema add

---
+++ backend/src/models/foodpartner.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const foodPartnerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const foodPartnerModle = mongoose.model("foodpartner", foodPartnerSchema)
+
+module.exports = foodPartnerModle;
